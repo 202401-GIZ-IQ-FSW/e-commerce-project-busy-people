f663f8d5c861249a0a545f7ea13298bd59bb1ced
ahmedj <ahmedarifusa@gmail.com>
2024-05-20 18:45:07 +0300

    added shope items and admin

--- server/models/shopItem.js
+++ server/models/shopItem.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 
 // Define Shop Item Schema
 const shopItemSchema = new mongoose.Schema({
-  title: { type: String, required: true },
+  title: { type: String, required: true ,trim: true},
   image: { type: String, required: true },
-  price: { type: Number, required: true },
-  description: { type: String, required: true },
-  availableCount: { type: Number, required: true },
+  price: { type: Number, required: true, min:0 },
+  description: { type: String, required: true,trim: true },
+  availableCount: { type: Number, required: true, min:0  },
   category: { type: String, required: true },
 });
 
